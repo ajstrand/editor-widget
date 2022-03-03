ea6a56349fe288178fb7c1e3c1fd2b268916a9c1
Alex <ajstrand8@gmail.com>
2022-03-02 22:58:06 -0500

    update example

--- example.js
+++ example.js
@@ -1,15 +1,12 @@
 #!/usr/bin/env node
-import pkg from "neo-blessed"
-const { Screen }= pkg;
-//var Editor = require('../.');
-
 import {fileURLToPath} from 'url';
+import pkg from "neo-blessed";
+import Editor from "./lib/Editor.js";
+
+const { Screen } = pkg;
 
 const __filename = fileURLToPath(import.meta.url);
 
-//const x = fileURLToPath("./hello.md")
-
-import Editor from "./lib/Editor.js";
 var screen = new Screen();
 screen.key('C-q', function () { process.exit(); });
 var editor = new Editor({parent: screen});
